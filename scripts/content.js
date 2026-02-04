1e87e8bcbb1f9fb54783d230c93ecbc1e10c4d09
Martin Tran <martintran2003@gmail.com>
2026-02-03 18:03:14 -0800

    support biwords

--- scripts/content.js
+++ scripts/content.js
@@ -3,8 +3,31 @@ const incorrectWordsSelector = ".replayWord";
 const completeObserver = new MutationObserver(() => {
   const target = document.querySelectorAll(incorrectWordsSelector); // select incorrect words
   if (target.length > 0) {
-    // save incorrect words to buffer
-    addToBuffer(Array.from(target).map((el) => el.innerText));
+    const incorrectWords = Array.from(target).map((el) => el.innerText);
+
+    // get the original text
+    const originalTextElement = document.querySelector("span.remainingChars");
+    const textToType = originalTextElement.innerText;
+
+    // Form biwords with incorrect words and previous word
+    const wordsArray = textToType.split(" ");
+    const biwords = [];
+    let startIndex = 0;
+    for (let i = 0; i < incorrectWords.length; i++) {
+      const incorrectWord = incorrectWords[i];
+      const wordIndex = wordsArray.indexOf(incorrectWord, startIndex);
+      // first word case
+      if (wordIndex > 0) {
+        const biword = wordsArray[wordIndex - 1] + " " + incorrectWord;
+        biwords.push(biword);
+      }
+      // start the scan after the current incorrect word
+      startIndex = wordIndex + 1;
+    }
+
+    // Save incorrect words and biwords to buffer
+    addToBuffer(incorrectWords);
+    addToBuffer(biwords);
 
     // reset the observer to look for a new game to start
     completeObserver.disconnect();
